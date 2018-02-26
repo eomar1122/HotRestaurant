2d4296498b1edbe13f8be09f3583366ba30b1fad
Michelle Garcia <michellemarshgarcia@gmail.com>
2018-02-26 13:57:46 -0700

    created server

---
+++ serve.js
@@ -0,0 +1,17 @@
+// Dependencies
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
